2a39ed4c5e2cf5698c539778c7ffdee3348344b5
Albert Camps <albert.camps@gmail.com>
2015-02-23 00:28:48 +0100

    First version.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+# This is a first commit, to get some points.
 
 ## Write a short comment describing this function
 
