790c5d33f863eecc87362b73d89c9f2d236b530c
Albert Camps <albert.camps@gmail.com>
2015-02-23 00:49:43 +0100

    Improve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -3,32 +3,32 @@
 # Generate structure for a cache.
 # In this case for cache inversion.
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    # x is the matrix to be inverted
+    cachedInverse <- NULL
     set <- function(y) {
-            x <<- y
-            m <<- NULL
+        x <<- y
+        cachedInverse <<- NULL
     }
     get <- function() x
-    setinverse <- function(inverse) m <<- inverse
-    getinverse <- function() m
+    setinverse <- function(inverse) cachedInverse <<- inverse
+    getinverse <- function() cachedInverse
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 # This function looks up for a matrix inverse, and if it
 # is not found, it calcules it and stores it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("Getting cached data")
+    } else {
         data <- x$get()
-        m <- solve(data)
-        x$setinverse(m)
-        m
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+    }
+    inverse
 }
